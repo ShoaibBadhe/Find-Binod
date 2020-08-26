ef7809319c22bfac60f072f0591dc44a7e7984a1
PatelFaiz <69766892+PatelFaiz@users.noreply.github.com>
2020-08-26 18:51:28 +0530

    Add files via upload

---
+++ Binod/Find_Binod.py
@@ -0,0 +1,26 @@
+import os
+dir_contents= os.listdir()
+# print(dir_contents)
+nBinod = 0
+def isBinod(filename):
+    with open(filename, 'r') as f:
+        filecontent = f.read()
+    if 'binod' in filecontent.lower():
+        return True
+        n+=1
+    else:
+        return False
+
+
+for item in dir_contents :
+    if item.endswith('txt'):
+        print (f'\nDetecting Binod in {item}')
+        flag = isBinod(item)
+        if flag:
+            print(f'Binod found in {item}')
+            nBinod += 1
+        else :
+            print(f'Binod is not found in {item}')
+        
+print("\n******Find Binod Summary******")
+print(f'{nBinod} files found with Binod into it!!!')
